d2b2441e6e1d96949911893a493554593abe1f66
Olya Kikot <kikotolya1989@gmail.com>
2021-07-15 15:09:38 +0300

    parcel

---
+++ src/index.js
@@ -0,0 +1,14 @@
+ import validatePassword from './js/validate.js';
+// console.log(validatePassword);
+
+
+// import validatePassword from './js/validate.js';
+// import {fetchAllUsers, fetchAllById, updateAllById, x as value} from './js/validate.js';
+
+// console.log(fetchAllUsers);
+// console.log(fetchAllById);
+// console.log(updateAllById);
+// console.log(value);
+// import * as apiService from './js/validate.js'
+
+// console.log(apiService);
---
+++ src/js/validate.js
@@ -0,0 +1,28 @@
+//  export default function validatePassword(password) {
+//     return true;
+// }
+
+// export const fetchAllUsers = () => {
+
+//     console.log('fetchAllUsers');
+// };
+
+// export const fetchAllById = () => {
+
+//     console.log('fetchAllById');
+// };
+
+// export const updateAllById = () => {
+
+//     console.log('updateAllById');
+// };
+
+// export const x = 5;
+
+
+import Joi from 'joi';
+console.log(Joi);
+
+ export default function validatePassword(password) {
+    return true;
+}
